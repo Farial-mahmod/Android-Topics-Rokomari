bcfaa59f15357b44264b37918d514d283428516f
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2021-10-31 12:39:31 +0600

    updating Main Activity

--- UI-Design/Bottom-Navigation/MainActivity.java
+++ UI-Design/Bottom-Navigation/MainActivity.java
@@ -1,1 +1,32 @@
-// under dev.
+package com.khalnayak.bottomnav;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initializing the Bottom Navigation
+        BottomNavigationView navView = findViewById(R.id.bottomNav_view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_favourites, R.id.navigation_profile, R.id.navigation_search )
+                .build();
+
+        // Initialize NavController.
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+}
